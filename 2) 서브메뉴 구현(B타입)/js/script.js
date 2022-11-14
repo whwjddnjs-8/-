d6aadea073378d141ee1dc8bb68f7433718b7f01
조정원 <whwjddnjs8@gmail.com>
2022-11-14 16:17:47 +0900

    서브메뉴구현(B타입)

--- 2) 서브메뉴 구현(B타입)/js/script.js
+++ 2) 서브메뉴 구현(B타입)/js/script.js
@@ -1,36 +1,7 @@
-$(function() {
-    /* Header (1)*/
-   /* $('.menu_area').mouseover(function() {
-        $('.sub_menu_area').stop().slideDown()
-    }).mouseout(function() {
-       $('.sub_menu_area').stop().slideUp()
-    })
-    
-    $('.sub_menu_area .menu_area ul.sub_menu').mouseover(function() {
-        var idx = $(this).index();
-        $('header .menu_area ul.main_menu li').eq(idx).children('a').addClass('active')
-    }).mouseout(function() {
-       $('header .menu_area ul.main_menu li a').removeClass('active')
-    })*/
-    
-    /* Header(2)*/
-   /* $('.menu_area').mouseover(function() {
-        $('.sub_menu_area').stop().slideDown()
-    }).mouseout(function() {
-        $('.sub_menu_area').stop().slideUp()
-    })
-    
-    $('.sub_menu_area .menu_area ul.sub_menu').mouseover(function() {
-        var idx = $(this).index()
-        $('header .menu_area ul.main_menu li').eq(idx).children('a').addClass('active')
-    }).mouseout(function() {
-        $('header .menu_area ul.main_menu li a').removeClass('active')
-    })*/
-    
-    /* Header(3)*/
-    $('.menu_area li').mouseover(function() {
-        $(this).children('.sub_menu').stop().slideDown()
-    }).mouseout(function() {
-        $(this).children('.sub_menu').stop().slideUp()
+$(function () {
+    $('.main_menu>li').mouseover(function () {
+        $('.sub_menu').stop().slideDown()
+    }).mouseout(function () {
+        $('.sub_menu').stop().slideUp()
     })
 })
